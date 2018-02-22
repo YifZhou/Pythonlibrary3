c01c3c6a75e5c1e7ea4ffbe182fec8fac300688a
Yifan Zhou <yifzhou@email.arizona.edu>
2018-02-21 17:34:08 -0700

    save.py

--- myio/save.py
+++ myio/save.py
@@ -1,16 +1,20 @@
 #! /usr/bin/env python
-"""use shelve to save parameters
+"""use pickle to save parameters
 """
 
 
-import shelve
+# import shelve
+import pickle
 
 def save(outputFN=None, **saveParams):
     if outputFN is None:
         print('No save file name given')
         return 1
-    db = shelve.open(outputFN)
-    for key in saveParams:
-        db[key] = saveParams[key]
-    db.close()
+    # db = shelve.open(outputFN)
+
+    # for key in saveParams:
+    #     db[key] = saveParams[key]
+    # db.close()
+    with open(outputFN, 'wb') as pkl:
+        pickle.dump(saveParams, pkl)
     return 0
