b30380ae529c320571f0dcd4e30ad6397ca5a706
YifZhou <zhouyifan1012@gmail.com>
2018-02-26 11:29:35 -0700

    re-wrote linear fit function using lmfit

--- fit/gaussFit.py
+++ fit/gaussFit.py
@@ -6,7 +6,7 @@ convienience function to fit a gaussian
 """
 
 
-def gaussFit(x, y, p0=None):
+def gaussFit(x, y, dy=None, p0=None):
     """
     x, y : variabile and measurement
     p0: inital guess for a, x0, sigma
@@ -14,8 +14,12 @@ def gaussFit(x, y, p0=None):
     mod = GaussianModel()
     pars = mod.guess(y, x=x)
     if p0 is not None:
-        pars['amplitude'].value = pars[0]
-        pars['center'].value = pars[1]
-        pars['sigma'].value = pars[2]
-    bestfit = mod.fit(y, x=x)
+        pars['amplitude'].value = p0[0]
+        pars['center'].value = p0[1]
+        pars['sigma'].value = p0[2]
+    if dy is not None:
+        weights = 1 / (dy * dy)
+    else:
+        weights = None
+    bestfit = mod.fit(y, x=x, params=pars, weights=weights)
     return bestfit
--- fit/linearFit.py
+++ fit/linearFit.py
@@ -1,29 +1,25 @@
-import numpy as np
-import matplotlib.pyplot as plt
+#! /usr/bin/env python
+from lmfit.models import LinearModel
 
 
-def linearFit(x, y, dy=None, doPlot=False, ax=None):
-    if dy is None:
-        dy = np.ones_like(y)
+def linearFit(x, y, dy=None, p0=None):
+    """ convenient function for linear fit using lmfit module
+
+    :param x: x parameter
+    :param y: y parameter
+    :param dy: uncertainties of y, default is None
+    :param p0: initial guess for the fitting parameters
+
     """
-    my own linear fit routine, since there is no good scipy or numpy linearFit routine written up
-    """
-    Y = np.mat(y).T
-    A = np.mat([np.ones(len(x)), x]).T
-    C = np.mat(np.diagflat(dy**2))
-    mat1 = (A.T * C**(-1) * A)**(-1)
-    mat2 = A.T * C**(-1) * Y
-    b, m = mat1 * mat2
-    b = b.flat[0]
-    m = m.flat[0]
-    sigb, sigm = np.sqrt(np.diag(mat1))
-    chisq = 1. / (len(x) - 2) * np.sum((y - m * x - b)**2 / dy**2)
-    if doPlot:
-        if ax is None:
-            fig = plt.figure()
-            ax = fig.add_subplot(111)
-        ax.plot(x, y, '.')
-        ax.plot((min(x), max(x)), (m*min(x) + b, m*max(x) + b))
-        ax.text(0.6, 0.1, r'$y={0:.2f}x + {1:.2f}$'.format(m, b),
-                transform=ax.transAxes)
-    return b, m, sigb, sigm, chisq
+    mod = LinearModel()
+    pars = mod.guess(y, x=x)
+    # if initial guesses are provided, over-write the auto-guessed result
+    if p0 is not None:
+        pars['intercept'].value = p0[0]
+        pars['slope'].value = p0[1]
+    if dy is not None:
+        weights = 1 / (dy * dy)
+    else:
+        weights = None
+    bestfit = mod.fit(y, x=x, params=pars, weights=weights)
+    return bestfit
